0b2aa3e4a8540353dea7ac6a8a0f7e85113cbb2d
Pomodor12 <167606195+Pomodor12@users.noreply.github.com>
2025-12-03 14:43:09 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from aiogram.filters import Command
 import sqlite3
 from datetime import datetime, timedelta
 
-TOKEN = 7999077800:AAGAlfz6ho1xAP2spR8k_18rGy4CPdWRo3k7999077800:AAGAlfz6ho1xAP2spR8k_18rGy4CPdWRo3k
+TOKEN = "7999077800:AAGAlfz6ho1xAP2spR8k_18rGy4CPdWRo3k7999077800:AAGAlfz6ho1xAP2spR8k_18rGy4CPdWRo3"
 
 GROUP_A = -5012773570
 
